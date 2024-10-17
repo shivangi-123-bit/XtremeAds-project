0b249468fb757a06b68c1790354024c8f996167f
Shivangi-128-bit <mishrashivangi3232@gmail.com>
2024-10-17 19:01:07 +0530

    functionality of case study section

--- script.js
+++ script.js
@@ -4,26 +4,61 @@ document.addEventListener("DOMContentLoaded", function () {
     smooth: true,
   });
 });
+function faqs() {
+  let Quests = document.querySelectorAll(".doubt-quest .ques .ques-ans h4");
+  let answers = document.querySelectorAll(".doubt-quest .ques .ques-ans p");
 
+  function fctnOfFaqs() {
+    Quests.forEach((question, index) => {
+      let arrow = question.querySelector("i");
 
+      question.addEventListener("click", () => {
+        const isAnswerVisible = answers[index].style.display === "block";
 
-let Quests = document.querySelectorAll(".doubt-quest .ques .ques-ans h4");
-let answers = document.querySelectorAll(".doubt-quest .ques .ques-ans p");
+        answers.forEach((answer, idx) => {
+          answer.style.display = "none";
+          Quests[idx].querySelector("i").classList.remove("rotate");
+        });
 
-Quests.forEach((question, index) => {
-  let arrow = question.querySelector("i");
-
-  question.addEventListener("click", () => {
-    const isAnswerVisible = answers[index].style.display === "block";
-
-    answers.forEach((answer, idx) => {
-      answer.style.display = "none";
-      Quests[idx].querySelector("i").classList.remove("rotate");
+        if (!isAnswerVisible) {
+          answers[index].style.display = "block";
+          arrow.classList.add("rotate");
+        }
+      });
     });
+  }
+  fctnOfFaqs();
+}
+faqs();
 
-    if (!isAnswerVisible) {
-      answers[index].style.display = "block";
-      arrow.classList.add("rotate");
-    }
-  });
-});
+function caseStudy() {
+  let topics = document.querySelectorAll(
+    ".case-study .flex-container .accordion .accordion-item button"
+  );
+  let content = document.querySelectorAll(
+    ".case-study .flex-container .accordion .accordion-item .accordion-content"
+  );
+
+  function fctnOfFaqs() {
+    topics.forEach((topic, index) => {
+      let arrow = topic.querySelector("i");
+
+      topic.addEventListener("click", () => {
+        const isContentVisible = content[index].style.display === "block";
+
+        content.forEach((tpc, idx) => {
+          tpc.style.display = "none";
+          topics[idx].querySelector("i").classList.remove("rotate");
+        });
+
+        if (!isContentVisible) {
+          content[index].style.display = "block";
+          arrow.classList.add("rotate");
+        }
+      });
+    });
+  }
+  fctnOfFaqs();
+}
+caseStudy();
+
