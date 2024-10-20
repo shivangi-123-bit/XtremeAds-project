9588dddc5fd924143307ebabfd313740b1035e8e
Shivangi-128-bit <mishrashivangi3232@gmail.com>
2024-10-21 00:25:44 +0530

    animating menu for smaller screen

--- script.js
+++ script.js
@@ -1,60 +1,10 @@
-function gsap() {
-  let tl = gsap.timeline();
-
-  gsap.from("section .hero", {
-    opacity: 0,
-    y: 10,
-    duration: 2,
-    stagger: 1,
-  });
-
-  gsap.from(".hero .hero-container .hero-content .left .box", {
-    x: -20,
-    opacity: 0,
-    delay: 1,
-    duration: 2,
-  });
-
-  let plans = document.querySelectorAll(".pricing-section .container2 .pricing-plans .plan");
-  let smoPlans = document.querySelectorAll(".smo-plans .plans-container .plan2");
-
-  plans.forEach((plan) => {
-    gsap.from(plan, {
-      opacity: 0,
-      transform: "rotate3D(0,500,0,180deg)",
-      x: -10,
-      duration: 2,
-      scrollTrigger: {
-        scrub: 5,
-        trigger: plan,
-        markers: false,
-        start: "top 70%",
-        end: "top 30%",
-      },
-    });
-  });
-
-  smoPlans.forEach((smoplan) => {
-    gsap.from(smoplan, {
-      opacity: 0,
-      transform: "rotate3D(0,500,0,180deg)",
-      x: -10,
-      duration: 2,
-      scrollTrigger: {
-        scrub: 5,
-        trigger: smoplan,
-        markers: false,
-        start: "top 80%",
-        end: "top 40%",
-      },
-    });
-  });
-}
-gsap();
-
 function caseStudy() {
-  let topics = document.querySelectorAll(".case-study .flex-container .accordion .accordion-item button");
-  let content = document.querySelectorAll(".case-study .flex-container .accordion .accordion-item .accordion-content");
+  let topics = document.querySelectorAll(
+    ".case-study .flex-container .accordion .accordion-item button"
+  );
+  let content = document.querySelectorAll(
+    ".case-study .flex-container .accordion .accordion-item .accordion-content"
+  );
 
   function fctnOfCaseStd() {
     topics.forEach((topic, index) => {
@@ -105,3 +55,94 @@ function faqs() {
   fctnOfFaqs();
 }
 faqs();
+
+document.addEventListener("DOMContentLoaded", function () {
+  let tl = gsap.timeline();
+  function animation() {
+    gsap.from("section .hero", {
+      opacity: 0,
+      y: 10,
+      duration: 2,
+      stagger: 1,
+    });
+
+    gsap.from(".hero .hero-container .hero-content .left .box", {
+      x: -20,
+      opacity: 0,
+      delay: 1,
+      duration: 2,
+    });
+
+    let plans = document.querySelectorAll(
+      ".pricing-section .container2 .pricing-plans .plan"
+    );
+
+    let smoPlans = document.querySelectorAll(
+      ".smo-plans .plans-container .plan2"
+    );
+
+    plans.forEach((plan) => {
+      gsap.from(plan, {
+        opacity: 0,
+        transform: "rotate3D(0,500,0,180deg)",
+        x: -10,
+        duration: 2,
+        scrollTrigger: {
+          scrub: 5,
+          trigger: plan,
+          markers: false,
+          start: "top 70%",
+          end: "top 30%",
+        },
+      });
+    });
+
+    smoPlans.forEach((smoplan) => {
+      gsap.from(smoplan, {
+        opacity: 0,
+        transform: "rotate3D(0,500,0,180deg)",
+        x: -10,
+        duration: 2,
+        scrollTrigger: {
+          scrub: 5,
+          trigger: smoplan,
+          markers: false,
+          start: "top 80%",
+          end: "top 40%",
+        },
+      });
+    });
+
+    // ------------------- for menu -----------------------
+
+    let menu = document.querySelector("nav .menu-icon");
+    let menuCross = document.querySelector(".menu-content .cross .menu-cross");
+
+    tl.to(".menu-content", {
+      right: 0,
+      duration: 0.7,
+    });
+
+    tl.from(".menu-content h4", {
+      x: 150,
+      opacity: 0,
+      stagger: 0.2,
+      duration: 0.4,
+    });
+
+    tl.from(menuCross, {
+      x: 100,
+      opacity: 0,
+    });
+
+    tl.pause();
+
+    menu.addEventListener("click", () => {
+      tl.play();
+    });
+    menuCross.addEventListener("click", () => {
+      tl.reverse();
+    });
+  }
+  animation();
+});
